a099d6c93ad25a81aab39b61be775309852f54d5
xxGw1997 <540040588@qq.com>
2022-11-16 17:59:20 +0800

    工厂模式

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject('xxgw') private readonly appService: AppService,
+    @Inject('test') private readonly shop: string[],
+    @Inject('factory') private readonly factory: number) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('shop')
+  getShop(): string[] {
+    return this.shop
+  }
+  
+  @Get('factory')
+  getFactory(): number {
+    return this.factory
+  }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppService2 } from './app.service2';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService2, {
+    provide: "xxgw",
+    useClass: AppService
+  }, {
+      provide: 'test',
+      useValue: ['TB', 'PDD', 'JD']
+    }, {
+      provide: 'factory',
+      inject: [AppService2],
+      useFactory(AppService2: AppService2) {
+        console.log(AppService2.getHello());
+        return 123
+      }
+    }],
 })
-export class AppModule {}
+export class AppModule { }
